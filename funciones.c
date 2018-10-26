d99bcda23a536d9614354b62599e8edc0d9b9b49
Andual <wlelouchx1gmail.com>
2018-10-26 10:40:54 -0400

    figuras

--- funciones.c
+++ funciones.c
@@ -1,10 +1,37 @@
 #include "stdio.h"
 #include "stdlib.h"
+
+int numerodelados(char f[]){
+	if (strcmp(f, "circulo")==0){
+		return 0;
+	}
+	else if(strcmp(f, "cuadrado")==0){
+		return 4;
+	}
+	else if(strcmp(f, "triangulo")==0){
+		return 3;
+	}
+	
+}
+
 int main()
 {
-	printf("hello \n" );
 	
-	system("pause");
+	int c;
+	char figura[20];
+
+	inicio:
+	
+	printf("Ingrese el nombre de la figura\n");
+	scanf("%s", figura);
+   
+    c = numerodelados(figura);
+    
+    printf("%d\n", c);
+
+    goto inicio;
 }
+
+
 
 
